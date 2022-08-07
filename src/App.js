c2e4bd47e560386115d43f4fab3680b5a0cafe71
Chirag Ramjibhai Patel <69707581+chiragramjibhaipatel@users.noreply.github.com>
2022-08-07 13:28:30 +0530

    rr

--- src/App.js
+++ src/App.js
@@ -2,6 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function handleClickGet() {
+    const url = "http://amazon.forever8.com:8080/inventory-summery/33e52852-a51d-40ad-9f90-21e81373872e?snapshotDateFrom=2022-07-01&snapshotDateTo=2022-07-31";
+    fetch(url, {
+      method: "GET",
+      headers: {"Content-type": "application/json;charset=UTF-8"}
+    })
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(err => console.log(err));
+  }
+
+  function handleClickPost() {
+    let _data = {
+      title: "foo",
+      body: "bar",
+      userId:1
+    }
+    const url = "http://amazon.forever8.com:8080/inventory-summery/33e52852-a51d-40ad-9f90-21e81373872e?snapshotDateFrom=2022-07-01&snapshotDateTo=2022-07-31";
+
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(_data),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(err => console.log(err));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +46,8 @@ function App() {
         >
           Learn React
         </a>
+      <button onClick={handleClickGet}>click me GET</button>
+      <button onClick={handleClickPost}>click me POST</button>
       </header>
     </div>
   );
